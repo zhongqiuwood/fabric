b3d361fc85637350fa419d92a6349e19c35d2483
mr404 <mr404@mr404deMacBook-Pro.local>
2018-03-30 15:05:49 +0800

    port 7506 to 7504 default

--- membersrvc/membership-cli/cmd/root.go
+++ membersrvc/membership-cli/cmd/root.go
@@ -68,7 +68,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cli.yaml)")
-	rootCmd.PersistentFlags().StringVar(&Url, "url", "localhost:7056", "url for server")
+	rootCmd.PersistentFlags().StringVar(&Url, "url", "localhost:7054", "url for server")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
